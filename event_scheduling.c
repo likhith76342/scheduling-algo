4616d9119dcbf6195057ced0d56622ba7e2e8d86
likhith76342 <likhithsomashekar@gmail.com>
2026-04-19 16:11:05 +0530

    Compare function implemented

--- event_scheduling.c
+++ event_scheduling.c
@@ -3,5 +3,22 @@
 #include <math.h>
 #include <time.h>
 
+typedef struct {
+	int start;
+	int end;
+}Event;
+
+/*Compare function (sorts using end time) */
+int compare(const void *a, const void *b) {
+    Event *e1 = (Event *)a;
+    Event *e2 = (Event *)b;
+
+    if (e1->end == e2->end) {
+        return e1->start - e2->start;
+    }
+    return e1->end - e2->end;
+}
+
 int main(){
+	
 }
